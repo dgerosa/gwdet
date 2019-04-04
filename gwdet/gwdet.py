fde39c99c351e03c9d86da429d3130538795bb5f
Davide Gerosa <dgerosa@caltech.edu>
2019-04-04 18:08:39 +0100

    v0.1.1

--- gwdet/gwdet.py
+++ gwdet/gwdet.py
@@ -25,7 +25,7 @@ import pathos.multiprocessing
 
 __author__ = "Davide Gerosa"
 __license__ = "MIT"
-__version__ = "0.1"
+__version__ = "0.1.1"
 __email__ = "dgerosa@caltech.edu"
 this_module='gwdet'
 
